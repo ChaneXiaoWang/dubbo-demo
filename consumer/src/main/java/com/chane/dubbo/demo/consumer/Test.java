dfd0a872bb035a45c1bbf175c2e206ae88d6d929
chanewang <378646744@qq.com>
2019-08-16 09:44:50 +0800

    Initial commit

---
+++ consumer/src/main/java/com/chane/dubbo/demo/consumer/Test.java
@@ -0,0 +1,14 @@
+package com.chane.dubbo.demo.consumer;
+
+/**
+ * @author chanewang
+ * @version 1.0
+ * @date 19/8/16
+ */
+public class Test {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
